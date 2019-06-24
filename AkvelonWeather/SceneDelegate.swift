863b765c8aafd77f4d070d237acd1cc2af68dbab
Arthur Mikhaylov <amikhaylov@swisstech.io>
2019-06-24 15:14:07 +0300

    Support for Mac added

--- AkvelonWeather/SceneDelegate.swift
+++ AkvelonWeather/SceneDelegate.swift
@@ -14,12 +14,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-
-        // Use a UIHostingController as window root view controller
-        let window = UIWindow(frame: UIScreen.main.bounds)
+        
+        #if targetEnvironment(UIKitForMac)
+            let windowScene = UIWindowScene(session: session, connectionOptions: connectionOptions)
+            let window = UIWindow(windowScene: windowScene)
+        #else
+            let window = UIWindow(frame: UIScreen.main.bounds)
+        #endif
+        
         window.rootViewController = UIHostingController(rootView: CityList())
         self.window = window
         window.makeKeyAndVisible()
